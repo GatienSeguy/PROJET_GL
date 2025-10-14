d6e3a588a1552b840d70d40cd247725f4bde5627
MaximeDegraeve <maxime.degraeve@ens-paris-saclay.fr>
2025-10-14 11:41:09 +0200

    v1 interface_locale maxime

--- INTERFACE/interface_local.py
+++ INTERFACE/interface_local.py
@@ -10,9 +10,46 @@ from tkinter import ttk
 
 
 # Créer la fenêtre principale
-fenetre = tk.Tk()
-fenetre.title("Ma première interface")
-fenetre.geometry("1000x600")  # largeur x hauteur
+class Fenetre(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.title("Paramétrage du Réseau de Neuronnes")
+        self.geometry("500x700")  # largeur x hauteur
+        #self.cadre = tk.Frame(self, borderwidth=50)
+        self.cadre = tk.Frame(self, borderwidth=50)
+        self.cadre.pack(fill="both", expand="yes")
+        self.CadreParams = tk.LabelFrame(self.cadre, text="Paramètres", borderwidth=2)
+        self.CadreParams.pack(fill="both", expand="yes")
+        tk.Button(self.CadreParams, text='Paramètres temporels et de découpage de données', height=3,command=self.Params_temporels).pack(fill="both",pady=10,padx=20)
+        tk.Button(self.CadreParams, text='Choix du modèle de réseau de neurones', height=3,command=self.Params_choix_reseau_neurones).pack(fill="both",pady=10,padx=20)
+        tk.Button(self.CadreParams, text="Paramétrage de l'architechture réseau", height=3,command=self.Params_archi_reseau).pack(fill="both",pady=10,padx=20)
+        tk.Button(self.CadreParams, text="Choix de la fonction perte (loss)", height=3,command=self.Params_choix_loss_fct).pack(fill="both",pady=10,padx=20)
+        tk.Button(self.CadreParams, text="Choix et paramétrage de l'optimisateur", height=3,command=self.Params_optimisateur).pack(fill="both",pady=10,padx=20)
+        tk.Button(self.CadreParams, text="Paramètres d'entrainement", height=3,command=self.Params_entrainement).pack(fill="both",pady=10,padx=20)
+        tk.Button(self.CadreParams, text="Paramétrage des métriques et visualisations de suivi", height=3,command=self.Params_visualisation_suivi).pack(fill="both",pady=10,padx=20)
+        
+
+
+    def Params_temporels(self):
+        pass
+    def Params_choix_reseau_neurones(self):
+        pass
+    def Params_archi_reseau(self):
+        pass
+    def Params_choix_loss_fct(self):
+        pass
+    def Params_optimisateur(self):
+        pass
+    def Params_entrainement(self):
+        pass
+    def Params_visualisation_suivi(self):
+        pass
+
+class Fenetre_Params(tk.Tk):
+    pass
+
+
 
 # # Ajouter un label
 # label = tk.Label(fenetre, text="Bonjour, Maxime !")
@@ -26,4 +63,5 @@ fenetre.geometry("1000x600")  # largeur x hauteur
 # bouton.pack()
 
 # Lancer la boucle principale
+fenetre = Fenetre()
 fenetre.mainloop()
