fc5ec795c862b7a0c1571ce092d0376f5d1f5923
MaximeDegraeve <maxime.degraeve@ens-paris-saclay.fr>
2025-10-14 08:53:26 +0200

    maxime

--- INTERFACE/interface_local.py
+++ INTERFACE/interface_local.py
@@ -1,21 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+# Paramètres et variables
+
+
+
+
+
+
 # Créer la fenêtre principale
 fenetre = tk.Tk()
 fenetre.title("Ma première interface")
-fenetre.geometry("300x200")  # largeur x hauteur
+fenetre.geometry("1000x600")  # largeur x hauteur
 
-# Ajouter un label
-label = tk.Label(fenetre, text="Bonjour, Maxime !")
-label.pack()
+# # Ajouter un label
+# label = tk.Label(fenetre, text="Bonjour, Maxime !")
+# label.pack()
 
-# Ajouter un bouton
-def clic():
-    label.config(text="Tu as cliqué!")
+# # Ajouter un bouton
+# def clic():
+#     label.config(text="Tu as cliqué!")
 
-bouton = tk.Button(fenetre, text="Clique-moi", command=clic)
-bouton.pack()
+# bouton = tk.Button(fenetre, text="Clique-moi", command=clic)
+# bouton.pack()
 
 # Lancer la boucle principale
 fenetre.mainloop()
