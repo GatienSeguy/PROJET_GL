c68578aed46b8c9be3450c140ef43d878cab3325
MaximeDegraeve <maxime.degraeve@ens-paris-saclay.fr>
2025-11-24 23:41:31 +0100

    aaaa

--- INTERFACE/interface_local_ctk.py
+++ INTERFACE/interface_local_ctk.py
@@ -19,7 +19,7 @@ import customtkinter as ctk
 
 # URL = "http://192.168.27.66:8000"
 # URL = "http://138.231.149.81:8000"
-URL = "http://192.168.1.94:8000"
+URL = "http://192.168.1.190:8000"
 
 # Paramètres et variables
 
