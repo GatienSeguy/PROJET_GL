2b1d92475105683e09f029c9427886115f170f53
soflecodeurfou <sofiane.boucherba@gmail.com>
2025-11-25 11:41:17 +0100

    mj

--- SERVEUR_DATA/TESTS/dataset_crop_test.py
+++ SERVEUR_DATA/TESTS/dataset_crop_test.py
@@ -1,19 +1,16 @@
 import requests
 import json
 
-# URL de ton serveur FastAPI (remplace le pcort si nécessaire)
-url = "http://127.0.0.1:8000/dataset/data_solo/"
+URL = "http://127.0.0.1:8000/datasets/info_all"
 
-# Envoi de la requête POST
+payload = {
+    "message": "choix dataset"
+}
 
-#Envoi d'un message pour demander EURO.json entre le 2020-01-01 et le 2020-06-01 avec un pas de 1 jour
-response = requests.post(url, json={"message": "choix dataset", "name": "EURO", "date_debut": "2020-01-01", "date_fin": "2020-06-01", "pas": "40d"})
+response = requests.post(URL, json=payload)
 
-
-# Vérification du code HTTP
-if response.status_code == 200:
-    # Affichage du JSON reçu de façon lisible
-    data = response.json()
-    print(json.dumps(data, indent=2))
-else:
-    print(f"Erreur {response.status_code}: {response.text}")
+print("Status code :", response.status_code)
+try:
+    print(json.dumps(response.json(), indent=2))
+except Exception:
+    print("Réponse brute :", response.text)
